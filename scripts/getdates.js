f0d4605376e3ec0ed433f5ea7bc00f7f8e809f6e
joobodo <joshgeenius@gmail.com>
2026-09-06 21:32:02 +0100

    added last modiified and current year script

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,3 +1,6 @@
+let date = new Date();
+
 let currentYear = document.querySelector("#currentyear");
 
-
+currentYear.innerHTML = date.getFullYear();
+document.getElementById("lastModified").textContent = "Last Modification: " + document.lastModified;

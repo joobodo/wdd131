1bbf5552ec70f4f97c25547649e88c4f9183e4f6
joobodo <joshgeenius@gmail.com>
2026-09-10 20:39:39 +0100

    fixed bom list

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,14 +1,27 @@
-const input = document.querySelector("#favchap");
-const button = document.querySelector("button");
-const list = document.querySelector("#list");
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
 
-const li = document.createElement("li");
-const deleteButton = document.createElement("button");
+button.addEventListener('click', function() {
+  if (input.value.trim() !== '') {
+    const li = document.createElement('li');
+    li.textContent = input.value;
 
-li.textContent = input.value;
+    const deleteButton = document.createElement('button');
 
-deleteButton.setAttribute("aria-label", "Close");
-deleteButton.setAttribute("id", "close-button");
-deleteButton.textContent = "❌";
+    deleteButton.setAttribute('aria-label', 'Close');
+    deleteButton.setAttribute('id', 'close-button');
+    deleteButton.textContent = '❌';
 
-list.append(li);
+    deleteButton.addEventListener('click', function () {
+      list.removeChild(li);
+      input.focus();
+    });
+    
+    li.append(deleteButton);
+    list.append(li);
+  }
+  else {
+    input.focus();
+  }
+});
